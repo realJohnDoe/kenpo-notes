6ee25795bd156503375e89d7a2b4a96fbea96bc3
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-09-23 15:22:44 +0200

    Update quartz to 4.5.2

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,38 +8,11 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/realJohnDoe/kenpo-notes",
-      Impressum: "https://realjohndoe.github.io/kenpo-notes/impressum",
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
-
-const defaultExplorer = Component.Explorer({
-  filterFn: (node) => {
-    // set containing names of everything you want to filter out
-    const omit = new Set(["numbers/index", "single-techniques/index", "web-of-knowledge/index", "impressum"])
-    return !omit.has(node.slug)
-  },
-  sortFn: (a, b) => {
-    // if (!a.isFolder && a.slug && !b.isFolder && b.slug) {
-    //   return a.slug.localeCompare(b.slug)
-    // }
-    // if (  !a.file && !b.file) {
-    //   return a.name.localeCompare(b.name)
-    // }
-
-    // Sort order: folders first, then files. Sort folders and files alphabetically
-    if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-      return a.slug.localeCompare(b.slug)
-    }
-
-    if (!a.isFolder && b.isFolder) {
-      return 1
-    } else {
-      return -1
-    }
-  }
-})
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -65,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    defaultExplorer,
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -89,7 +62,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    defaultExplorer,
+    Component.Explorer(),
   ],
   right: [],
 }
