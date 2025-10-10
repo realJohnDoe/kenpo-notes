808eac143661cd63afc37056a1ff8a2208bc62b5
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-10-10 11:29:19 +0200

    Fix errors about svelte config

--- animations/svelte.config.js
+++ animations/svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { readdirSync } from 'fs';
-import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const getYamlSlugs = () => {
-  const srcFormsDir = join(process.cwd(), 'src', 'forms');
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+  const srcFormsDir = join(currentDir, 'src', 'forms');
   const ymlFiles = readdirSync(srcFormsDir).filter((file) => file.endsWith('.yml'));
   return ymlFiles.map((file) => `/animation/${file.replace('.yml', '')}`);
 };
